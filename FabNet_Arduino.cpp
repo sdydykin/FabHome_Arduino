3ba77f650c52cf4b08dd848f3b89a1c004af1ee4
Dydykin Sergey <sdydykin@yandex.ru>
2024-04-07 14:18:03 +0300

    Initial commit
    Добавлено описание системы. Добавлен верхний уровень ПО.

---
+++ FabNet_Arduino.cpp
@@ -0,0 +1,47 @@
+#include "FabNet_Arduino.h"
+
+//Конструктор класса
+FabNet_Arduino::FabNet_Arduino(SoftwareSerial& serial, unsigned char re_pin, unsigned char de_pin) {
+  //Сохраняем выводы управления драйвером сети FabNet
+  _re_pin = re_pin; 
+  _de_pin = de_pin;
+
+  //Сохраняем последовательный порт для доступа к сети FabNet
+  _serial = serial;
+}
+    
+//Настройка аппаратной конфигурации
+void FabNet_Arduino::begin(void) {
+  //Настраиваем пины управления драйвером сети FabNet
+  pinMode(_re_pin, OUTPUT);
+  pinMode(_de_pin, OUTPUT);
+
+  //Переводим драйвер в режим приема
+  digitalWrite(_re_pin, LOW);
+  digitalWrite(_de_pin, LOW);
+} 
+    
+//Чтение вещественной переменной
+bool FabNet_Arduino::read_float_var(unsigned int dev_id, unsigned char var_id, float * var_val) {
+  return true;
+}
+
+//Чтение целочисленной переменной со знаком
+bool FabNet_Arduino::read_int_var(unsigned int dev_id, unsigned char var_id, long * var_val) {
+  return true;
+}
+
+//Чтение целочисленной перемпенной без знака
+bool FabNet_Arduino::read_uint_var(unsigned int dev_id, unsigned char var_id, unsigned long * var_val) {
+  return true;
+}
+
+//Чтение логической переменной
+bool FabNet_Arduino::read_bool_var(unsigned int dev_id, unsigned char var_id, bool * var_val) {
+  return true;
+}
+
+//Запись логической переменной
+bool FabNet_Arduino::write_bool_var(unsigned int dev_id, unsigned char var_id, bool var_val) {
+  return true;
+}
---
+++ FabNet_Arduino.h
@@ -0,0 +1,35 @@
+#ifndef FABNET_ARDUINO_H
+#define FABNET_ARDUINO_H
+
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+class FabNet_Arduino {
+  private:
+    //Выводы для управления драйвером сети FabNet
+    unsigned char _re_pin; 
+    unsigned char _de_pin;
+
+    //Последователный порт для доступа к сети FabNet
+    SoftwareSerial& _serial;
+  public:
+    //Конструктор класса
+    FabNet_Arduino(SoftwareSerial& serial, unsigned char re_pin, unsigned char de_pin);
+
+    //Настройка аппаратной конфигурации
+    void begin(void);
+    
+    //Чтение вещественной переменной
+    bool read_float_var(unsigned int dev_id, unsigned char var_id, float * var_val);
+    //Чтение целочисленной переменной со знаком
+    bool read_int_var(unsigned int dev_id, unsigned char var_id, long * var_val);
+    //Чтение целочисленной перемпенной без знака
+    bool read_uint_var(unsigned int dev_id, unsigned char var_id, unsigned long * var_val);
+    //Чтение логической переменной
+    bool read_bool_var(unsigned int dev_id, unsigned char var_id, bool * var_val);
+
+    //Запись логической переменной
+    bool write_bool_var(unsigned int dev_id, unsigned char var_id, bool var_val);
+};
+
+#endif
